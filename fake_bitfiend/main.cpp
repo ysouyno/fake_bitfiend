be4212e1f2bf5f089d36beaaec9119682315c797
ysouyno <ysouyno@163.com>
2017-08-28 14:45:46 +0800

    Add win32 pthread configuration

--- fake_bitfiend/main.cpp
+++ fake_bitfiend/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <pthread.h>
+
+void *pthread_thread_proc(void *arg)
+{
+	std::cout << "hello pthread!" << std::endl;
+
+	return NULL;
+}
 
 int main()
 {
-	std::cout << "hello world!" << std::endl;
+	pthread_t tid;
+
+	pthread_create(&tid, NULL, pthread_thread_proc, NULL);
+	pthread_join(tid, NULL);
 
 	return 0;
 }
